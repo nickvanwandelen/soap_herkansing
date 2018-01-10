60db93a384677d7cbec943e735f771ad97c1aa5f
Nick <nick.vanwandelen@hotmail.com>
2018-01-10 22:14:48 +0100

    Fixed bugs with BMI calculation Fixed bug with not finding the operation
    Added commentary

--- src/main/java/nl/hu/iac/soap/impl/BmiServiceImpl.java
+++ src/main/java/nl/hu/iac/soap/impl/BmiServiceImpl.java
@@ -6,17 +6,28 @@ import javax.jws.WebService;
 
 @WebService(endpointInterface = "nl.hu.iac.soap.wsinterface.BmiServiceInterface")
 public class BmiServiceImpl implements BmiServiceInterface{
+    /** Calculates the BMI for the SOAP service based on the height and length of the user.
+     * The BMI calculation is done via this formula:
+     * BMI = weight / (length * length)
+     *
+     *
+     * @param req incoming SOAP request
+     * @return the calculated BMI in a double format
+     * @throws Fault any error that can occur during the calculation is thrown
+     */
     @Override
     public BmiResponse calculateBmi(BmiRequest req) throws Fault {
         try{
             BmiResponse resp = new BmiResponse();
-            resp.setResult(req.getWeight().doubleValue() / (req.getHeight().intValue() * (req.getHeight().intValue() * req.getHeight().intValue())));
+            double height = req.getHeight().doubleValue() / 100; //Divide the given height by 100 to convert to meters instead of centimeters.
+
+            resp.setResult(req.getWeight().doubleValue() / (height * height)); //calculate the BMI
             return resp;
         }
         catch(Exception e){
             BmiFault bmiFault = new BmiFault();
             bmiFault.setErrorCode((short) 1);
-            throw new Fault("Something went horribly wrong while calculating your BMI! No offence...", bmiFault);
+            throw new Fault("Something went horribly wrong while calculating your BMI! No offence..." + "\n Error: " + e.getMessage(), bmiFault);
         }
     }
 }
